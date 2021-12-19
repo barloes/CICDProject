5d4f83bea41ae233a00818bf8b290269e9036f81
barloes <junhuitee@hotmail.com>
2021-12-20 01:18:37 +0800

    new ci to push image

---
+++ .github/push_image.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 642151248908.dkr.ecr.ap-southeast-1.amazonaws.com
+ecr_name=$1 #junhuiimage
+image_name=$2 #cicdfypfe
+
+aws_acc=642151248908.dkr.ecr.ap-southeast-1.amazonaws.com
+docker build -t $image_name ./fe
+docker tag $ecr_name:$image_name $aws_acc/$ecr_name:$image_name
+docker push $aws_acc/$ecr_name:$image_name

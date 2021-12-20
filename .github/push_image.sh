4c78dcebb8fbed00296492e1ee00f78113d56443
barloes <junhuitee@hotmail.com>
2021-12-20 15:05:18 +0800

    update docker compose name

--- .github/push_image.sh
+++ .github/push_image.sh
@@ -3,8 +3,7 @@
 aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 642151248908.dkr.ecr.ap-southeast-1.amazonaws.com
 ecr_name=$1 #junhuiimage
 image_name=$2 #cicdfypfe
-
 aws_acc=642151248908.dkr.ecr.ap-southeast-1.amazonaws.com
-docker build -t $image_name ./fe
-docker tag $ecr_name:$image_name $aws_acc/$ecr_name:$image_name
+
+docker build -t $aws_acc/$ecr_name:$image_name ./fe
 docker push $aws_acc/$ecr_name:$image_name
